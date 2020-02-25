3324a49e2266d2f4dd5a6fe184f8f347a23957c0
xinzheshen <xinzheshen@163.com>
2020-02-25 12:27:29 +0800

    use generic views

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,22 @@ from . import views
 # 设置命名空间app_name，以便于在模板中指定对应空间下的url名称
 app_name = 'polls'
 
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+# 使用通用视图，改良urlconf。
+# 第二个和第三个匹配准则中，路径字符串中匹配模式的名称已经由 <question_id> 改为 <pk>  (即primary_key)
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,46 +2,66 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 # Create your views here.
 
 
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    # # 使用数据库API
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
+# def index(request):
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#     # # 使用数据库API
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#
+#     # # 使用模板
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+#
+#     # 「载入模板，填充上下文，再返回由它生成的 HttpResponse 对象」是一个非常常用的操作流程。
+#     # 于是 Django 提供了一个快捷函数 render（），
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     # return HttpResponse("You're looking at question %s." % question_id)
+#
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#
+#     # 尝试用 get() 函数获取一个对象，如果不存在就抛出 Http404 错误也是一个普遍的流程。
+#     # Django 也提供了一个快捷函数get_object_or_404()
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
-    # # 使用模板
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
+# 删除旧的 index, detail, 和 results 视图，并用 Django 的通用视图代替。
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    # 「载入模板，填充上下文，再返回由它生成的 HttpResponse 对象」是一个非常常用的操作流程。
-    # 于是 Django 提供了一个快捷函数 render（），
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
 
-    # 尝试用 get() 函数获取一个对象，如果不存在就抛出 Http404 错误也是一个普遍的流程。
-    # Django 也提供了一个快捷函数get_object_or_404()
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def results(request, question_id):
